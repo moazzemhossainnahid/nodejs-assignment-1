f553944d74c0fee6e8f00a7db5d011d41271defb
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-10-26 20:37:09 +0600

    get all users

--- Routes/V1/users.route.js
+++ Routes/V1/users.route.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const usersControllers = require('../../Controllers/users.controllers');
 const router = express.Router();
+const usersData = require("../../UserData.json");
 
-
-// router.route("")
+// get all users
+router.get("/all", (req, res) => {
+    const limit = req.query.limit;
+    if (limit) {
+        const result = usersData.slice(0, limit)
+        res.send(result)
+    } else {
+        res.send(usersData)
+    }
+})
 
 
 
