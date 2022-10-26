fb3b2a00d561d2541c15001e2e1bdad98652e76d
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-10-26 19:14:23 +0600

    add err handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const usersRoutes = require("./Routes/V1/users.route");
+const errorHandler = require('./Middlewares/errorHandler');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,8 +25,19 @@ app.all("*", (req, res) => {
     res.send("No Results Found");
 });
 
+app.use(errorHandler);
+
 app.listen(port, () => {
     console.log("Listen to Port", port);
+});
+
+
+// Express Unhandled Error
+process.on("unhandledRejection", (error) => {
+    console.log(error.name, error.message);
+    app.close(() => {
+        process.exit(1)
+    })
 });
 
 
