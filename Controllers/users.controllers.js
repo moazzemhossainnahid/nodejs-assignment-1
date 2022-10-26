48ee7e2875fc887dd0c5baa0b5273859ffb037d1
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-10-26 20:46:38 +0600

    add random user

--- Controllers/users.controllers.js
+++ Controllers/users.controllers.js
@@ -1,3 +1,21 @@
-module.exports.getAllUsers = (req, res) => {
+const usersData = require("../UserData.json");
 
+
+// get all users
+module.exports.getAllUsers = (req, res) => {
+    const limit = req.query.limit;
+    if (limit) {
+        const result = usersData.slice(0, limit)
+        res.send(result)
+    } else {
+        res.send(usersData)
+    }
+}
+
+
+// get random user
+module.exports.getRandomUser = (req, res) => {
+    const randomNumber = Math.floor(Math.random() * usersData.length);
+    const randUser = usersData.find(user => user.index == randomNumber)
+    res.send(randUser);
 }
--- Routes/V1/users.route.js
+++ Routes/V1/users.route.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const usersControllers = require('../../Controllers/users.controllers');
 const router = express.Router();
-const usersData = require("../../UserData.json");
 
 // get all users
-router.get("/all", (req, res) => {
-    const limit = req.query.limit;
-    if (limit) {
-        const result = usersData.slice(0, limit)
-        res.send(result)
-    } else {
-        res.send(usersData)
-    }
-})
+router.get("/all", usersControllers.getAllUsers);
+
+// get random user
+router.get("/random", usersControllers.getRandomUser);
 
 
 
